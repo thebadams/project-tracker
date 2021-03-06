0ca14c0060132d6b311a045415c39aeb93251244
Brian Adams <bpeteradams@gmail.com>
2021-03-06 14:32:46 -0500

    wrote function to display content - hard coded

--- script.js
+++ script.js
@@ -13,8 +13,27 @@ function getFormValues(){
     var projectNameValue = $("#project-name").val();
     var projectTypeValue = $("#projet-type").val();
     var hourlyWageValue = $("#hourly-wage").val();
-    var dueDateValue = $("#due-date");
+    var dueDateValue = $("#due-date").val();
     window[`project${projectNum}`] = new projectInformation(projectNameValue, projectTypeValue, hourlyWageValue, dueDateValue);
-    projectNum++
+    
+}
+function calculateEarnings(){
+
+}
+
+function displayContent() {
+    tableBody.append("<tr></tr>");
+    $("tbody tr").append("<td class = projectValue></td>");
+    $("tbody tr").append("<td class = projectType></td>");
+    $("tbody tr").append("<td class = projectWage></td>");
+    $("tbody tr").append("<td class = projectDue></td>");
+    $("tbody tr").append("<td class = projectEarnings></td>");
+    $("td.projectValue").text(project1.projectName);
+    $("td.projectType").text(project1.projectType);
+    $("td.projectWage").text(project1.hourlyWage);
+    $("td.projectDue").text(project1.dueDate);
+
+
+
     
 }
